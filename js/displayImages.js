a460cf5c0c3c95c7b4bab282696bc774ad340a87
RoryLetteney <rory.letteney@gmail.com>
2019-04-17 09:58:54 -0500

    Finished draft 1 of generateImage

--- js/displayImages.js
+++ js/displayImages.js
@@ -2,12 +2,28 @@
 
 $(document).ready(() => {
   const doc = $(document);
-  const generateImage = (imageUrl, title, description, keyword, horns) => {
+  const generateImage = (image) => {
     const imageContainer = doc.createElement('section');
     const imageTitle = doc.createElement('h2');
-    const image = doc.createElement('img');
+    const imageImg = doc.createElement('img');
     const imageDescription = doc.createElement('p');
 
-    // $(imageContainer).attr();
+    $(imageContainer).addClass('image');
+    $(imageContainer).attr({
+      'data-keyword': image.keyword,
+      'data-horns': image.horns
+    });
+    $(imageTitle).text(image.title);
+    $(imageImg).attr({
+      src: image.imageUrl,
+      alt: image.title
+    });
+    $(imageDescription).text(image.description);
+
+    imageContainer.appendChild(imageTitle.cloneNode());
+    imageContainer.appendChild(imageImg.cloneNode());
+    imageContainer.appendChild(imageDescription.cloneNode());
+    $('main').appendChild(imageContainer);
   };
+  Image.all.forEach(image => generateImage(image));
 });
