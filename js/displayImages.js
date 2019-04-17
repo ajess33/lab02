9fd677a3251a7ab60b6836bb63f522cb4441f0a5
RoryLetteney <rory.letteney@gmail.com>
2019-04-17 18:48:50 -0500

    Removed sortImages.js and placed sorting logic in filterImages.js. Moved generateImage to global scope. Sorting based upon title and amount of horns.

--- js/displayImages.js
+++ js/displayImages.js
@@ -1,28 +1,25 @@
 'use strict';
 
-$(document).ready(() => {
-  const generateImage = (image) => {
-    const imageContainer = document.createElement('section');
-    const imageTitle = document.createElement('h2');
-    const imageImg = document.createElement('img');
-    const imageDescription = document.createElement('p');
+const generateImage = (image) => {
+  const imageContainer = document.createElement('section');
+  const imageTitle = document.createElement('h2');
+  const imageImg = document.createElement('img');
+  const imageDescription = document.createElement('p');
 
-    $(imageContainer).addClass('image');
-    $(imageContainer).attr({
-      'data-keyword': image.keyword,
-      'data-horns': image.horns
-    });
-    $(imageTitle).text(image.title);
-    $(imageImg).attr({
-      src: image.imageUrl,
-      alt: image.title
-    });
-    $(imageDescription).text(image.description);
+  $(imageContainer).addClass('image');
+  $(imageContainer).attr({
+    'data-keyword': image.keyword,
+    'data-horns': image.horns
+  });
+  $(imageTitle).text(image.title);
+  $(imageImg).attr({
+    src: image.imageUrl,
+    alt: image.title
+  });
+  $(imageDescription).text(image.description);
 
-    imageContainer.appendChild(imageTitle.cloneNode(true));
-    imageContainer.appendChild(imageImg.cloneNode(true));
-    imageContainer.appendChild(imageDescription.cloneNode(true));
-    document.getElementsByTagName('main')[0].appendChild(imageContainer);
-  };
-  Image.all.forEach(image => generateImage(image));
-});
+  imageContainer.appendChild(imageTitle.cloneNode(true));
+  imageContainer.appendChild(imageImg.cloneNode(true));
+  imageContainer.appendChild(imageDescription.cloneNode(true));
+  document.getElementsByTagName('main')[0].appendChild(imageContainer);
+};
--- js/filterImages.js
+++ js/filterImages.js
@@ -14,16 +14,37 @@ $(document).ready(() => {
     'json'
   ).done(() => options.forEach(option => $(filterOption).clone().attr('value', option).text(option).appendTo('#filter optgroup:first')));
 
+  const sortImages = (filterValue) => {
+    if (filterValue === 'a-to-z') {
+      Image.all.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (filterValue === 'z-to-a') {
+      Image.all.sort((a, b) => b.title.localeCompare(a.title));
+    } else if (filterValue === 'high-to-low') {
+      Image.all.sort((a, b) => b.horns - a.horns);
+    } else if (filterValue === 'low-to-high') {
+      Image.all.sort((a, b) => a.horns - b.horns);
+    }
+  };
+
+  sortImages('a-to-z');
+  Image.all.forEach(image => generateImage(image));
+
   filter.change(() => {
     let selectedOption = filter.val();
-    Object.values($('.image')).forEach(image => {
-      if (selectedOption === 'default') {
-        $(image).show();
-      } else if ($(image).attr('data-keyword') !== $('#filter option:selected').text() && filter[0].selectedIndex < $('#filter option').length - 4) {
-        $(image).hide();
-      } else {
-        $(image).show();
-      }
-    });
+    if (filter[0].selectedIndex < $('#filter option').length - 4) {
+      Object.values($('.image')).forEach(image => {
+        if (selectedOption === 'default') {
+          $(image).show();
+        } else if ($(image).attr('data-keyword') !== $('#filter option:selected').text() && filter[0].selectedIndex < $('#filter option').length - 4) {
+          $(image).hide();
+        } else {
+          $(image).show();
+        }
+      });
+    } else {
+      $('main').html('');
+      sortImages(filter.val());
+      Image.all.forEach(image => generateImage(image));
+    }
   });
 });
