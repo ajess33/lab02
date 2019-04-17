4e64e91ed8d01cd37b1ed523742020fb78e8f073
RoryLetteney <rory.letteney@gmail.com>
2019-04-17 09:43:18 -0500

    Built dynamic scaffolding for displayImages

---
+++ js/displayImages.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(() => {
+  const doc = $(document);
+  const generateImage = (imageUrl, title, description, keyword, horns) => {
+    const imageContainer = doc.createElement('section');
+    const imageTitle = doc.createElement('h2');
+    const image = doc.createElement('img');
+    const imageDescription = doc.createElement('p');
+
+    // $(imageContainer).attr();
+  };
+});
