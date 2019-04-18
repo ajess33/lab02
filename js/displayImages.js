beb206ce665923e26d78c9734dae3bf9cceb3936
Austin Jess <ajess33@gmail.com>
2019-04-18 11:03:35 -0500

    page now pulls from template

--- js/displayImages.js
+++ js/displayImages.js
@@ -1,25 +1,43 @@
-'use strict';
+('use strict');
 
 const generateImage = (image) => {
-  const imageContainer = document.createElement('section');
-  const imageTitle = document.createElement('h2');
-  const imageImg = document.createElement('img');
-  const imageDescription = document.createElement('p');
+  // const imageContainer = document.createElement('section');
+  // const imageTitle = document.createElement('h2');
+  // const imageImg = document.createElement('img');
+  // const imageDescription = document.createElement('p');
 
-  $(imageContainer).addClass('image');
-  $(imageContainer).attr({
-    'data-keyword': image.keyword,
-    'data-horns': image.horns
-  });
-  $(imageTitle).text(image.title);
-  $(imageImg).attr({
-    src: image.imageUrl,
-    alt: image.title
-  });
-  $(imageDescription).text(image.description);
+  // $(imageContainer).addClass('image');
+  // $(imageContainer).attr({
+  //   'data-keyword': image.keyword,
+  //   'data-horns': image.horns
+  // });
+  // $(imageTitle).text(image.title);
+  // $(imageImg).attr({
+  //   src: image.imageUrl,
+  //   alt: image.title
+  // });
+  // $(imageDescription).text(image.description);
 
-  imageContainer.appendChild(imageTitle.cloneNode(true));
-  imageContainer.appendChild(imageImg.cloneNode(true));
-  imageContainer.appendChild(imageDescription.cloneNode(true));
-  document.getElementsByTagName('main')[0].appendChild(imageContainer);
+  // imageContainer.appendChild(imageTitle.cloneNode(true));
+  // imageContainer.appendChild(imageImg.cloneNode(true));
+  // imageContainer.appendChild(imageDescription.cloneNode(true));
+  // document.getElementsByTagName('main')[0].appendChild(imageContainer);
+
+  // get template data from html
+  const template = $('#image-template').html();
+
+  // compile template data into a function
+  const templateScript = Handlebars.compile(template);
+
+  const imageData = {
+    title: image.title,
+    image_url: image.imageUrl,
+    description: image.description,
+    horns: image.horns
+  };
+
+  console.log(imageData);
+
+  let html = templateScript(imageData);
+  $('main').append(html);
 };
