c7f76087ae26a717ec405f7dab01e4748ecfaa28
RoryLetteney <rory.letteney@gmail.com>
2019-04-18 10:11:17 -0500

    Added when.then to app.js and moved the pagination nav rendering to app.js.

--- js/app.js
+++ js/app.js
@@ -3,25 +3,39 @@
 Image.all = [];
 
 // populate the images on page load
-$.get(
-  './data/page1.json',
-  (data) => {
-    data.forEach((image) => {
-      new Image(image);
-    });
-  },
-  'json'
-);
+$.when(
+  $.get(
+    './data/page1.json',
+    (data) => {
+      data.forEach((image) => {
+        new Image(image);
+      });
+    },
+    'json'
+  ),
 
-$.get(
-  './data/page2.json',
-  (data) => {
-    data.forEach((image) => {
-      new Image(image);
-    });
-  },
-  'json'
-);
+  $.get(
+    './data/page2.json',
+    (data) => {
+      data.forEach((image) => {
+        new Image(image);
+      });
+    },
+    'json'
+  )
+).then(() => {
+  const pagNav = $('#image-pages');
+  const page = document.createElement('a');
+  $(page).addClass('page-link');
+  for (let i = 0; i < Math.ceil(Image.all.length / 10); i++) {
+    $(page).text((i+1));
+    $(page).attr('href', `#${$(page).text()}`);
+    $(pagNav).append(page.cloneNode(true));
+  }
+});
+
+
+
 
 function Image(image) {
   this.imageUrl = image.image_url;
--- js/pagination.js
+++ js/pagination.js
@@ -1,12 +1,5 @@
 'use strict';
 
-$(document).ready(() => {
-  const pagNav = $('#image-pages');
-  const page = document.createElement('a');
-  $(page).addClass('page-link');
-  for (let i = 0; i < Math.ceil(Image.all.length / 10); i++) {
-    $(page).text((i+1));
-    $(page).attr('href', `#${$(page).text()}`);
-    $(pagNav).append(page.cloneNode(true));
-  }
-});
+const paginate = () => {
+  $('main')[0].html('');
+};
